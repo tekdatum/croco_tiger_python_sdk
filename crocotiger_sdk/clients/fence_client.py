77765a1a538f9a01702f3c53be58c2a33bd2aa9a
rolandotkd <rolando@tekdatum.com>
2026-01-29 16:27:33 -0600

    fence client

---
+++ crocotiger_sdk/clients/fence_client.py
@@ -0,0 +1,12 @@
+from models.fence_validation import FenceValidation
+from utils.rest import RestClient
+
+
+class FenceClient:
+    def __init__(self, rest_client: RestClient):
+        self._rest_client = rest_client
+        self._endpoint = "/fence"
+
+    def validate(self, project_id: int, text: str) -> FenceValidation:
+        data = self._rest_client.post(f"{self._endpoint}/validate/${project_id}", data={"text": text})
+        return FenceValidation(**data)
