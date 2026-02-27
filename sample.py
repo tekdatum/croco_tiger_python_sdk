f9444c3bc25849aa70119262a87908e0ef1dbc63
Javier Perez <thxuapp@gmail.com>
2026-02-27 15:25:34 -0600

    DF-224 update sample

--- sample.py
+++ sample.py
@@ -6,18 +6,6 @@ client = SDK(base_path="http://localhost:8000/api/v1")
 # Load the project
 project_client = client.get_project_client()
 project = project_client.find_one_by_name(Project.MEDICARE.value)
-print(project)
-
-project_client.update(
-    project_id=project.id,
-    name=project.name,
-    topic=project.topic,
-    restricted_topics=project.restricted_topics,
-    total_topic_questions=project.total_topic_questions,
-    url=project.url,
-    zip=project.zip,
-    context="Any context",
-)
 
 # Validate text for a specific project (e.g., project_id=1)
 fence_client = client.get_fence_client()
